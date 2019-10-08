4f6f12b9539820e8b368a219bb1db5f5b7adba10
William Staniscia <william.staniscia@outlook.com>
2019-10-08 13:18:18 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+def print_display_rainbow(colors)
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  
+end
